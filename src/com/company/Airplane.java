5a48d0a0761e4559875fdb0439e9ba256a3963f8
1asset <82859085+1asset@users.noreply.github.com>
2021-10-05 00:38:06 +0600

    Create Airplane.java

---
+++ src/com/company/Airplane.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Airplane implements Air_transport {
+
+    @Override
+    public void fly() {
+        System.out.println("Air transport type: Airplane");
+    }
+}
