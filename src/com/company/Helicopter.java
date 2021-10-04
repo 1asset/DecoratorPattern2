d4bb9f2ec0fe0469ea143419485ffef1a3d8ea18
1asset <82859085+1asset@users.noreply.github.com>
2021-10-05 00:38:34 +0600

    Create Helicopter.java

---
+++ src/com/company/Helicopter.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Helicopter implements Air_transport {
+
+    @Override
+    public void fly() {
+        System.out.println("Air transport type: Helicopter");
+    }
+}
